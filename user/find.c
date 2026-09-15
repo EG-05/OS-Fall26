c8193d5fe3958281472c812a12e7a65d54f89c3d
Eshaal <seshaalahmed.gardezi24@lyceumschool.edu.pk>
2026-09-15 19:43:48 +0500

    Add regex support to find

--- user/find.c
+++ user/find.c
@@ -4,6 +4,45 @@
 #include "kernel/param.h"
 #include "user/user.h"
 
+int matchhere(char *re, char *text);
+int matchstar(int c, char *re, char *text);
+
+int
+match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
+}
+
 void
 runcmd(char **cmdv, int cmdc, char *file)
 {
@@ -51,7 +90,7 @@ find(char *path, char *name, char **execv, int execc)
     if (*q == '/')
       base = q + 1;
 
-  if (strcmp(base, name) == 0) {
+  if (match(name, base)) {
     if (execv)
       runcmd(execv, execc, path);
     else
